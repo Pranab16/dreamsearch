d96131ed90d2e10e9270d9c943a82e0f1a2b96f0
Pranab Gupta <pranab.gupta@joshtechnologygroup.com>
2015-03-24 00:59:40 +0530

    Minor changes

--- app/indices/product_index.rb
+++ app/indices/product_index.rb
@@ -1,6 +1,5 @@
 ThinkingSphinx::Index.define :product, :with => :active_record do
   indexes name, :sortable => true
-  indexes category.name, :as => :author, :sortable => true
 
   has :id, :category_id
 end
